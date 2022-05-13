a347d4bf286171ade0103efc9e79a23f8dfe0a92
mbarnestech <maggie.barnes@gmail.com>
2022-05-12 23:13:17 -0600

    add computer player and error message

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,12 +1,35 @@
+print("ROCK, PAPER, SCISSORS!")
+intro_q = input("How many players? Enter \"1\" or \"2\" ")
+
+
 user1 = input("First Player: rock, paper, or scissors? ")
 
-print("NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING")
+if int(intro_q) == 2:
 
-user2 = input("Second Player: rock, paper, or scissors? ")
+	print("NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING")
+
+	user2 = input("Second Player: rock, paper, or scissors? ")
+
+elif int(intro_q) == 1:
+	import random
+	computer = random.randint(1, 3)
+	if computer == 1:
+		user2 = "rock"
+		print(f"Computer: {user2}")
+	elif computer == 2:
+		user2 = "paper"
+		print(f"Computer: {user2}")
+	else:
+		user2 = "scissors"
+		print(f"Computer: {user2}")
+else: 
+	print("Sorry, please try again")
 
 if (user1 == "rock" and user2 == "scissors") or (user1 == "paper" and user2 == "rock") or (user1 == "scissors" and user2 == "paper"):
 	print("First Player Wins!")
 elif(user2 == "rock" and user1 == "scissors") or (user2 == "paper" and user1 == "rock") or (user2 == "scissors" and user1 == "paper"):
 	print("Second Player Wins!")
-else:
+elif user1 == user2:
 	print("It's a tie!")
+else:
+	print("Something went wrong. Typo?")
