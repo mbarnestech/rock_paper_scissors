930a455fe967b6769abb63d735488533104255dd
mbarnestech <maggie.barnes@gmail.com>
2022-05-17 16:42:59 -0600

    add more quit options and reduce possible typo problems

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -9,7 +9,7 @@ while replay == "y": #keeps going until user says they don't want to play or res
 	intro_q = int(intro_q)
 
 	#how many rounds
-	out_of = input("How many rounds would you like to play? Enter a number. " )
+	out_of = input("How many rounds would you like to play? Enter a number: " )
 	out_of = int(out_of)
 
 	#setting/resetting wins to 0
@@ -20,35 +20,38 @@ while replay == "y": #keeps going until user says they don't want to play or res
 	for n in range(out_of): #repeats game the number of times the user requested above
 		
 		#Sequence for player input
-		user1 = input("First Player: rock, paper, or scissors? ").lower() #user1 input for both 1 and 2 player versions.
+		user1 = input("First Player: rock, paper, or scissors? ").lower()[0] #user1 input for both 1 and 2 player versions.
 
-		if intro_q == 2: #code to hide first player's input and allow second player to type in choice
+		if user1 == "q":
+			break
+
+		elif intro_q == 2: #code to hide first player's input and allow second player to type in choice
 
 			print("NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING \n \n NO CHEATING")
 
-			user2 = input("Second Player: rock, paper, or scissors? ").lower()
+			user2 = input("Second Player: rock, paper, or scissors? ").lower()[0]
 
 		elif intro_q == 1: #code for generated input from a computer
 			import random
 			computer = random.randint(1, 3)
 			if computer == 1:
 				user2 = "rock"
-				print(f"Computer: {user2}")
 			elif computer == 2:
 				user2 = "paper"
-				print(f"Computer: {user2}")
 			else:
 				user2 = "scissors"
-				print(f"Computer: {user2}")
+			print(f"Computer: {user2}")
+			user2 = user2[0]
+
 		else: #for if the user entered an invalid introq - exits program.
 			print("Sorry, you did not type \"1\" or \"2\". Please restart and try again.")
 			break
 
 		#Sequence to determine who wins round
-		if (user1 == "rock" and user2 == "scissors") or (user1 == "paper" and user2 == "rock") or (user1 == "scissors" and user2 == "paper"):
+		if (user1 == "r" and user2 == "s") or (user1 == "p" and user2 == "r") or (user1 == "s" and user2 == "p"):
 			print("First Player Wins!")
 			first_player_wins += 1 # adding to win counter
-		elif(user2 == "rock" and user1 == "scissors") or (user2 == "paper" and user1 == "rock") or (user2 == "scissors" and user1 == "paper"):
+		elif(user2 == "r" and user1 == "s") or (user2 == "p" and user1 == "r") or (user2 == "s" and user1 == "p"):
 			if intro_q == 1:
 				print("Computer Wins!")
 			else:
@@ -69,8 +72,12 @@ while replay == "y": #keeps going until user says they don't want to play or res
 		if intro_q == 1: #for one player game
 			print(f"Computer won {second_player_wins} rounds and wins the game!")
 	else:
-		print(f"It's a draw!")
+		print("Nobody wins - the game is a draw!")
 
-	replay = input("Well done! Would you like to play again? (y/n) ").lower() #ends game if anything but "y" or "Y" are entered.
+	if user1 == "q":
+		replay = "n"
+	else:
+		replay = input("Would you like to play again? (y/n) ").lower()[0] #ends game if anything but "y" or "Y" are entered.
+	#replay = replay[0]
 
 print("Thank you for playing! Goodbye!")
