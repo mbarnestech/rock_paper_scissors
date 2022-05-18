aa57169a6ba1b741d10092116f96b87ff0eef538
mbarnestech <maggie.barnes@gmail.com>
2022-05-18 16:38:15 -0600

    Add user2name variable & add final scores

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -7,6 +7,11 @@ while replay == "y": #keeps going until user says they don't want to play or res
 	# how many players
 	intro_q = input("How many players? Enter \"1\" or \"2\": ")
 	intro_q = int(intro_q)
+	# setting future text to either "Second Player" or "Computer"
+	if intro_q == 1:
+		user2name = "Computer"
+	else:
+		user2name = "Second Player"
 
 	#how many rounds
 	out_of = input("How many rounds would you like to play? Enter a number: " )
@@ -49,13 +54,10 @@ while replay == "y": #keeps going until user says they don't want to play or res
 
 		#Sequence to determine who wins round
 		if (user1 == "r" and user2 == "s") or (user1 == "p" and user2 == "r") or (user1 == "s" and user2 == "p"):
-			print("First Player Wins!")
+			print("First Player wins the round!")
 			first_player_wins += 1 # adding to win counter
 		elif(user2 == "r" and user1 == "s") or (user2 == "p" and user1 == "r") or (user2 == "s" and user1 == "p"):
-			if intro_q == 1:
-				print("Computer Wins!")
-			else:
-				print("Second Player Wins!")
+			print(f"{user2name} wins the round!")
 			second_player_wins += 1 # adding to win counter
 		elif user1 == user2:
 			print("It's a tie!")
@@ -65,14 +67,13 @@ while replay == "y": #keeps going until user says they don't want to play or res
 
 	#sequence to determines who wins game
 	if first_player_wins > second_player_wins:
-		print (f"First player won {first_player_wins} rounds and wins the game!")
+		print ("First Player wins the game!")
 	elif second_player_wins > first_player_wins:
-		if intro_q == 2: #for two player game
-			print (f"Second player won {second_player_wins} rounds and wins the game!")
-		if intro_q == 1: #for one player game
-			print(f"Computer won {second_player_wins} rounds and wins the game!")
+		print(f"{user2name} wins the game!")
 	else:
 		print("Nobody wins - the game is a draw!")
+
+	print(f"Final Scores: \n First Player: {first_player_wins} \n {user2name}: {second_player_wins} \n Ties: {ties}")
 
 	if user1 == "q":
 		replay = "n"
