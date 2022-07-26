94f7b363f83b7a977b0981acf8378ea85c22b321
mbarnestech <maggie.barnes@gmail.com>
2022-07-26 16:56:43 -0600

    overwrite accidental commit message - this is a palindrome checker

--- exercises/50.py
+++ exercises/50.py
@@ -14,4 +14,4 @@ print(is_palindrome('testing')) # False
 print(is_palindrome('tacocat')) # True
 print(is_palindrome('hannah')) # True
 print(is_palindrome('robert')) # False
-print(is_palindrome('amanaplanacanalpanama'))  # True
+print(is_palindrome('a man a plan a canal panama'))  # True
