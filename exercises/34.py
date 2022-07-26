48bce2e5d4016b4196a1cb344e13a7542456e385
mbarnestech <maggie.barnes@gmail.com>
2022-07-25 22:26:36 -0600

    add 2 answers for exercise 34

---
+++ exercises/34.py
@@ -0,0 +1,6 @@
+# make sure your solution is assigned to the answer variable so the tests can work!
+answer = {}.fromkeys('aeiou', 0)
+print(answer)
+
+alternateanswer = {char:0 for char in 'aeiou'}
+print(alternateanswer)
