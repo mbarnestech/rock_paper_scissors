8346f54332d9f8a06bfcd5d1efeb08056947ee3c
mbarnestech <maggie.barnes@gmail.com>
2022-07-26 11:18:54 -0600

    fix count function

---
+++ exercises/41.py
@@ -0,0 +1,17 @@
+# # Without adding any new lines of code, make count_dollar_signs work as intended
+# def count_dollar_signs(word):
+#     count = 0
+#     for char in word:
+#         if char == '$':
+#             count += 1
+#         return count
+
+def count_dollar_signs(word):
+    count = 0
+    for char in word:
+        if char == '$':
+            count += 1
+    return count
+
+
+print(count_dollar_signs("sd$sdlfkjs$sdklfjsldviuu$sdf$"))
