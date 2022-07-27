972500fc20c64778ca993ac4360a8591fb2308ee
mbarnestech <maggie.barnes@gmail.com>
2022-07-26 18:09:40 -0600

    make function to capitalize first letter of name

---
+++ exercises/53.py
@@ -0,0 +1,7 @@
+def capitalize(first_name):
+    """Will capitalize first letter of argument first_name"""
+    return first_name[0].upper() + first_name[1:]
+
+
+print(capitalize("tim")) # "Tim"
+print(capitalize("matt")) # "Matt"
