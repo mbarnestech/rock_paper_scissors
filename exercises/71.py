58a5b9afaf9271da7955c0bec3edc79cf7d7860b
mbarnestech <maggie.barnes@gmail.com>
2022-08-04 15:56:09 -0600

    update with comments

--- exercises/71.py
+++ exercises/71.py
@@ -1,5 +1,7 @@
 def extract_full_name(nameset):
+	# First Try:
 	# return list(map(lambda x: (x.get("first") + " " + x.get("last")), nameset))
+	#After checking solution, turns out the .get() was unecessary - this is a little shorter:
 	return list(map(lambda x: (x["first"] + " " + x["last"]), nameset))
 
 names = [{'first': 'Elie', 'last': 'Schoppik'}, {'first': 'Colt', 'last': 'Steele'}]
